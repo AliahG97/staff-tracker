0f651df077d43e9ee132374221b3adfed4876212
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-02-09 15:56:01 -0500

    inital commit

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
+
+USE staff_db;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL   
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL, 
+    salary DECIMAL NOT NULL,
+    dept_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY(dept_id)
+    REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, 
+    CONSTRAINT fk_role FOREIGN KEY(role_id)
+    REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY(manager_id)
+    REFERENCES employee(id) ON DELETE SET NULL
+);
