3b8780dead631ea09f9a69ba1d4739dec0fe4eac
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-02-13 18:10:34 -0500

    cases are all added to  index.js  still needs functions to view each case.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+// Include packages needed for this application
+const inquirer = require("inquirer");
+// Display table in the terminal
+const consoleTable = require('consolle.Table');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -21,3 +22,61 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the staff_db database.`)
 );
+// Start application arrow method function
+const startApp = () => {
+  inquierer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View all departments':
+          viewDepartment();
+          break;
+
+      case 'View all roles':
+        viewRoles();
+        break;
+
+      case 'View all employees':
+        viewEmployees();
+        break;
+
+      case 'Add a department':
+        addDepartment();
+        break;
+
+      case 'Add a role':
+        addRole();
+        break;
+      
+      case 'Add an employee':
+        addEmployee();
+        break;
+      
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+
+      case 'Exit':
+        db.end();
+        break;
+
+      default:
+        console.log(`Invalid actions: ${answer.action}`);
+        break;
+    }
+  });
+};
